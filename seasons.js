0c796b65a447a92377b61ec696cc0ce142f53321
Trudy Gill <trudzzg@gmail.com>
2022-02-01 22:26:30 -0400

    add first javaScript file

---
+++ seasons.js
@@ -0,0 +1,12 @@
+let season = 'winter';
+let message = '';
+
+if (season == 'spring') {
+    message = 'It is springtime!';
+} else if (season === 'summer') {
+    message = 'It is summertime!';
+} else {
+    message = 'It is neither summer nor spring!';
+}
+
+console.log(message);
