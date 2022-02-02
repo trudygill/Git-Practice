956e3aed4c11078f41861be4d80954f716050d17
Trudy Gill <trudzzg@gmail.com>
2022-02-01 22:32:47 -0400

    edit comparison operator on line 6

--- seasons.js
+++ seasons.js
@@ -3,7 +3,7 @@ let message = '';
 
 if (season == 'spring') {
     message = 'It is springtime!';
-} else if (season === 'summer') {
+} else if (season == 'summer') {
     message = 'It is summertime!';
 } else {
     message = 'It is neither summer nor spring!';
